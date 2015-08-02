28f30dd67d31acbdc80a52ed2041ae27792c5856
inhakki <inhakki@gmail.com>
2015-08-02 08:58:58 -0400

    unknown database schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529024440) do
+ActiveRecord::Schema.define(version: 20140530054803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140529024440) do
   create_table "memberships", force: true do |t|
     t.integer "game_id"
     t.integer "user_id"
+    t.boolean "is_ready", default: false
   end
 
   create_table "movies", force: true do |t|
